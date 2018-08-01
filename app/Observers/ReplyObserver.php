34613ffefdb56615ae576a78f1148745603817e2
vagrant <vagrant@homestead>
2018-08-01 05:20:45 +0000

    Notify author when topic replied

--- app/Observers/ReplyObserver.php
+++ app/Observers/ReplyObserver.php
@@ -3,6 +3,8 @@
 namespace App\Observers;
 
 use App\Models\Reply;
+use App\Notifications\TopicReplied;
+
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -11,7 +13,12 @@ class ReplyObserver
 {
     public function created(Reply $reply)
     {
+
+        $topic = $reply->topic;
+
         $reply->topic->increment('reply_count', 1);
+        // 通知作者话题被回复了
+        $topic->user->notify(new TopicReplied($reply));
     }
 
     public function creating(Reply $reply)
