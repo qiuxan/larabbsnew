bb95c2e24eb43ff763a921ba66d3138a1caf99e4
vagrant <vagrant@homestead>
2018-08-15 00:38:47 +0000

    user update

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Models\Image;
 use App\Transformers\UserTransformer;
 use App\Http\Requests\Api\UserRequest;
 
@@ -41,8 +42,27 @@ class UsersController extends Controller
             ->setStatusCode(201);
     }
 
+    public function update(UserRequest $request)
+    {
+        $user = $this->user();
+
+        $attributes = $request->only(['name', 'email', 'introduction']);
+
+        if ($request->avatar_image_id) {
+            $image = Image::find($request->avatar_image_id);
+
+            $attributes['avatar'] = $image->path;
+        }
+        $user->update($attributes);
+
+        return $this->response->item($user, new UserTransformer());
+    }
+
+
     public function me()
     {
         return $this->response->item($this->user(), new UserTransformer());
     }
+
+
 }
